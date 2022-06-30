30980d4d4a1a4df3946ede5f1cb616d0c3cfb5af
abhinav <99832265+Abhinav80555@users.noreply.github.com>
2022-06-30 06:20:55 +0000

    payment failed

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -1,28 +1,52 @@
 const express = require("express");
-const router =express.Router();
-const Booking=require("../models/bookingModel");
-const Product=require("../models/productModel");
+const router = express.Router();
+const Booking = require("../models/bookingModel");
+const Product = require("../models/productModel");
+const { v4: uuidv4 } = require('uuid');
+const stripe = require('stripe')('sk_test_51LCG0gSFmd6Rsl6r4qYWAIZiD1qnPd5kY7Iz0EQamRkRbuG01ZnWSd7ef8H39tksQAvLTnPHiq6FUnGfdiIxiWjX00gyG2Uj9Q')
 
 
-router.post("/bookproduct",async(req,res)=>{
+router.post("/bookproduct", async (req, res) => {
 
-req.body.transactionId ='1234'
+
+  const { token } = req.body
   try {
-    const newbooking = new Booking(req.body)
-    await newbooking.save()
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id
+    })
 
-    
-const product = await Product.findOne({_id : req.body.product})
-    product.bookedTimeSlots.push(req.body.bookedTimeSlots)
-    console.log(req.body.product)
-    await product.save()
-    
-    res.send('your booking is successfull')
+    const payment = await stripe.charges.create({
+      amount: req.body.totalAmount * 100,
+      currency: 'INR',
+      customer: customer.id,
+      receipt_email: token.email
+    }, {
+      idempotencyKey: uuidv4(),
+    })
+
+    if (payment) {
+      req.body.transactionId = payment.source.id
+      const newbooking = new Booking(req.body)
+      await newbooking.save()
+
+
+      const product = await Product.findOne({ _id: req.body.product })
+      product.bookedTimeSlots.push(req.body.bookedTimeSlots)
+      console.log(req.body.product)
+      await product.save()
+
+      res.send('your booking is successfull')
+    } else {
+      return res.status(400).json(error);
+    }
+
+
   } catch (error) {
     return res.status(400).json(error);
   }
-  
+
 });
 
 
-module.exports=router
+module.exports = router
